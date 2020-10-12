65dd7765666da11a18c62418cfad66ce77470288
rushi2509 <rushikeshshibe000@gmail.com>
2020-10-12 23:24:22 +0530

    2nd cpp code Added

---
+++ xyz.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello Everyone we'll Do Addition This time";
+	int a=15, b=25, sum=0;
+	sum=a+b;
+	cout<<"\nAddditon :: "<<sum<<endl;
+
+}
